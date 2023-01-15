f0be8d2ea4cd13db6639e939df5aff86f5f39595
unknown <fredericreact@gmail.com>
2023-01-16 00:36:00 +0800

    Testing User Interaction & State

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,5 +1,6 @@
 import Greeting from "./Greeting";
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 
 describe('Greeting compoenent', () => {
 
@@ -17,6 +18,42 @@ describe('Greeting compoenent', () => {
         
         });
 
+    test('renders good to see you if the button is not clicked', () => {
+        render(<Greeting/>);
 
+        const outputElement = screen.getByText('good to see you', {exact: false})
+        
+        expect(outputElement).toBeInTheDocument();
+    });
+
+    test('renders changed if the button is clicked', () => {
+        
+        // 1.Arrange
+        render(<Greeting/>);
+        
+        // 2. Act
+        const buttonElement = screen.getByRole('button')
+        userEvent.click(buttonElement)
+        // 3. Assert
+        const outputElement = screen.getByText('Changed')
+        expect(outputElement).toBeInTheDocument();
+        
+        
+    })
+
+    test('does not render good to see you if the button was clicked', () => {
+        
+        // 1.Arrange
+        render(<Greeting/>);
+        
+        // 2. Act
+        const buttonElement = screen.getByRole('button')
+        userEvent.click(buttonElement)
+        // 3. Assert
+        const outputElement = screen.queryByText('good to see you', {exact: false})
+        expect(outputElement).toBeNull()
+        
+        
+    })
 })
 
